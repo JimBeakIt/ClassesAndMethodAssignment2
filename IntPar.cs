1556cf75426e805389c3289da9987d182d58d398
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-20 10:42:15 +0000

    update

---
+++ IntPar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodAssignment2
+{
+    public class IntPar
+    {
+        public int uNum1 { get; set; }
+        public int uNum2 { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace ClassesAndMethodAssignment2
 {
-    class Program
+    public class Program
     {
+        public static int Data1 { get; set; }
+        public static int Data2 { get; set; }
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            string uNum1 = Console.ReadLine();
+            Data1 = Int32.Parse(uNum1);
+
+            Console.WriteLine("Optional: Enter another number:");
+            string uNum2 = Console.ReadLine();
+            Data2 = Int32.Parse(uNum2);
+
+
+            int result = Data1 + 6;
+            Console.WriteLine("The result is " + result);
+            Console.ReadLine();
         }
     }
 }
+
