91f63002ee815c1879eabc6adee830ec00cb0c65
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2022-01-16 11:31:22 +0000

    update

--- IntPar.cs
+++ IntPar.cs
@@ -8,7 +8,9 @@ namespace ClassesAndMethodAssignment2
 {
     public class IntPar
     {
-        public int uNum1 { get; set; }
-        public int uNum2 { get; set; }
+        public int NumU(int Num1, int Num2 = 5)
+        {
+            return Num1 + Num2;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,17 +12,20 @@ namespace ClassesAndMethodAssignment2
         public static int Data2 { get; set; }
         static void Main(string[] args)
         {
+            var result = new IntPar();
+
             Console.WriteLine("Enter a number:");
             string uNum1 = Console.ReadLine();
-            Data1 = Int32.Parse(uNum1);
+            Data1 = Convert.ToInt32(uNum1);
 
             Console.WriteLine("Optional: Enter another number:");
             string uNum2 = Console.ReadLine();
+            if (!string.IsNullOrEmpty(uNum2))
             Data2 = Int32.Parse(uNum2);
 
 
-            int result = Data1 + 6;
-            Console.WriteLine("The result is " + result);
+            var result2 = result.NumU(Num1: Data1, Num2: Data2);
+            Console.WriteLine("The result is " + result2);
             Console.ReadLine();
         }
     }
